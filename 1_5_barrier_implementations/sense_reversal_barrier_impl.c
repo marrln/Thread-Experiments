cc8001aa1ed8fff0572fc69c51ed9456cc87c7b6
Maria Eleni Patsi <mrlnpts@outlook.com.gr>
2025-12-05 17:01:30 +0200

    Revert changes in the sense barrier since it did not help

--- 1_5_barrier_implementations/sense_reversal_barrier_impl.c
+++ 1_5_barrier_implementations/sense_reversal_barrier_impl.c
@@ -20,19 +20,24 @@ void sense_barrier_init(sense_barrier_t *barrier, int count) {
 
 void sense_barrier_wait(sense_barrier_t *barrier, int *local_sense) {
     *local_sense = !(*local_sense);
-    int my_sense = *local_sense;
     
     pthread_mutex_lock(&barrier->mutex);
     barrier->waiting++;
     
     if (barrier->waiting == barrier->count) {
         barrier->waiting = 0;
-        barrier->sense = my_sense;
+        barrier->sense = *local_sense;
         pthread_mutex_unlock(&barrier->mutex);
     } else {
+        int my_sense = *local_sense;
         pthread_mutex_unlock(&barrier->mutex);
-        while (barrier->sense != my_sense) {
-            // Busy wait - reads are safe without lock
+        while (1) {
+            pthread_mutex_lock(&barrier->mutex);
+            if (barrier->sense == my_sense) {
+                pthread_mutex_unlock(&barrier->mutex);
+                break;
+            }
+            pthread_mutex_unlock(&barrier->mutex);
         }
     }
 }
