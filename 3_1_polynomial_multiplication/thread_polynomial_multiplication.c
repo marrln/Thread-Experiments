d24da25be2f1aab0be6ab702edbffa9785a38088
Maria Eleni Patsi <mrlnpts@outlook.com.gr>
2026-01-05 14:47:14 +0200

    Fix usage message to include number of threads in polynomial multiplication

--- 3_1_polynomial_multiplication/thread_polynomial_multiplication.c
+++ 3_1_polynomial_multiplication/thread_polynomial_multiplication.c
@@ -9,9 +9,9 @@ int main(int argc, char *argv[]) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	if (argc != 2) {
+	if (argc < 2 || argc > 3) {
 		if (rank == 0)
-			printf("Usage: %s <degree>\n", argv[0]);
+			printf("Usage: %s <degree> <num_threads>\n", argv[0]);
 		MPI_Finalize();
 		return 1;
 	}
