c9e0623f39ad7afb9f519b79ccd87b5b1b75bda1
Maria Eleni Patsi <mrlnpts@outlook.com.gr>
2026-02-16 17:18:56 +0200

    Update which degrees to run

--- 4_1_polynomial_multiplication/run_poly_mult.sh
+++ 4_1_polynomial_multiplication/run_poly_mult.sh
@@ -12,7 +12,7 @@ if [ ! -f "$CSV_FILE" ]; then
     echo "degree,time_alloc,time_init,time_seq,time_simd,time_verify,time_cleanup,time_total,verification,user" > "$CSV_FILE"
 fi
 
-for degree in 5000 20000 80000 100000 200000 500000; do
+for degree in 5000 20000 80000 100000; do
     echo "Degree: $degree"
     output=$(./bin/simd_poly_mult $degree)
     echo "$output"
